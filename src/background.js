7fade93277a62eac1a5561783493f3b4fcd9f090
Max White <maxwell.oliver.white@gmail.com>
2018-04-13 21:00:00 -0700

    comments, added state logic variables

--- src/background.js
+++ src/background.js
@@ -1,10 +1,14 @@
-// this is the background code...
-
-// listen for our browerAction to be clicked
+// this background code defines the logic for the popup and injects our scripts
 
 
+// keeps track of which character we want to infect the page
 var clickedChar = "ant";
 
+// keeps track of whether scripts have been injected
+var ready = false;
+
+
+// sets clickedChar to appropriate character, injects scripts
 function click(e) {
   clickedChar = e.path[0].className;
 
@@ -22,20 +26,7 @@ function click(e) {
   window.close();
 }
 
-// function injectScripts() {
-//   chrome.tabs.executeScript(null,
-//     {file: "jquery-3.3.1.min.js"});
-//   chrome.tabs.executeScript(null,
-//     {file: "./src/rotate.js"});
-//     chrome.tabs.executeScript(null,
-//       {file: "./src/scrapewords.js"});
-//   chrome.tabs.executeScript(null,
-//     {file: "./src/main.js"});
-//   chrome.tabs.insertCSS(tabId, {
-//      file : "./src/styles.css"
-//     });
-// }
-
+// dunno if I ever call this
 function chooseChar(e) {
   console.log(e.path[0].className);
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -48,7 +39,7 @@ function chooseChar(e) {
   });
 }
 
-// this is unique to our popup
+// adds click listener to each character div
 document.addEventListener('DOMContentLoaded', function () {
   var divs = document.querySelectorAll('div');
   for (var i = 0; i < divs.length; i++) {
@@ -56,6 +47,8 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 });
 
+
+// waits for scripts to run, then sends main.js the ok to start with new char
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "ready_to_infect" ) {
